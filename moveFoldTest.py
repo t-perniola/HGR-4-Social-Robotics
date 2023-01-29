14297f2a827af937043fa10ad0be85b787f758d9
t-perniola <t.perniola1@studenti.uniba.it>
2023-01-29 20:10:44 +0100

    fix
    fatto in modo che vengano estratti tutti i keypoints da ogni video

--- moveFoldTest.py
+++ moveFoldTest.py
@@ -1,11 +1,44 @@
 import os
 
-DATADIR = r"C:\Users\follo\OneDrive\Desktop\test"
+SOURCE = r"C:\Users\follo\OneDrive\Desktop\test"
+TEST = r"C:\Users\follo\OneDrive\Desktop\test\vid1"
+DESTINATION = r"C:\Users\follo\OneDrive\Desktop\test\gest"
+
+
+lsit = [5,3,1,8]
+sublsit = lsit[0:3]
+
+print(sublsit)
+
+
+
+
+
+
+videotest = ["vid1","vid2", "vid3"]
+
+for vid in videotest: #per ogni label (per ogni gesto)
+    try:
+        os.makedirs(os.path.join(SOURCE, vid))
+    except:
+        pass #se esistono già, skippa la creazione
+
 
 gestures = ["ciao", "bella", "zio"]
 
 for label in gestures: #per ogni label (per ogni gesto)
     try:
-        os.makedirs(os.path.join(DATADIR, label))
+        os.makedirs(os.path.join(DESTINATION, label))
     except:
         pass #se esistono già, skippa la creazione
+
+
+allvid = os.listdir(SOURCE) 
+
+# iterate on all files to move them to destination folder
+for vid in allvid:
+    if not vid == "gest":       
+        src_path = os.path.join(SOURCE, vid)
+        dst_path = os.path.join(DESTINATION, label, vid)
+        os.rename(src_path, dst_path)
+
